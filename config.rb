e800f303e312ac438ebcdccc2a61619e4a11450e
GATAKAWAKACHICO <area.aria@gmail.com>
2018-08-26 04:15:32 +0900

    Updated homepage

---
+++ config.rb
@@ -0,0 +1,9 @@
+
+css_dir = "assets/css" # by Compass.app 
+sass_dir = "src/styles" # by Compass.app 
+images_dir = "assets/img" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = true # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
